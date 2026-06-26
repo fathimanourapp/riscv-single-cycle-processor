88d1639a8a8eb4a17779916fc182d9fcf299a9b9
fathimanourapp <fathimanourapp@gmail.com>
2026-06-26 20:45:13 +0530

    processor.v

--- rtl/processor.v
+++ rtl/processor.v
@@ -86,29 +86,3 @@ wire Zero = (ALUResult == 32’b0);
 assign pc_branch = pc + ImmExt;
 assign pc_next = (Branch && Zero) ? pc_branch : pc_plus4;
 endmodule
-module riscV_processor_tb;
-reg clk;
-riscV_processor dut (
-.clock(clk)
-);
-initial begin
-clk = 0;
-forever #5 clk = ~clk;
-end
-initial begin
-dut.pc = 32’d0;
-dut.regis.rf[9] = 32’h00000010; // x9 base address
-dut.regis.rf[5] = 32’h00000005;
-// x5
-dut.regis.rf[6] = 32’h00000000; // x6
-dut.regis.rf[4] = 32’h00000000; // x4
-dut.regis.rf[8] = 32’h00000000; // x8
-// lw x6,-4(x9)
-dut.dmem.d_mem[12] = 8’b00010100;
-dut.dmem.d_mem[13] = 8’b00000000;
-dut.dmem.d_mem[14] = 8’b00000000;
-dut.dmem.d_mem[15] = 8’b00000000;
-#250;
-$stop;
-end
-endmodule
